f54a5ee3501e6eae4b18080e89c5da2096402ace
Lucas Pinheiro <lps1704@gmail.com>
2023-04-03 12:39:13 -0300

    create loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import {ClipLoader} from 'react-spinners'
+
+const Loading = () => {
+  return (
+    <>
+      <ClipLoader />
+    </>
+  )
+}
+
+export default Loading
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,8 +2,9 @@
 
 import axios from 'axios';
 import {  useSearchParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import ProductCard from '../components/ProductCard';
+import React, { useCallback, useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard'
+import Loading from '../loading'
 
 interface ProductsType   {
   id: number,
@@ -21,27 +22,47 @@ interface ProductsType   {
 
 const page = () => {
   const [data, setData] = useState<ProductsType[]>([])
-
+  const [isLoading, setIsLoading] = useState(false)
   const searchParams = useSearchParams();
 
   const search = searchParams.get('q');
 
+  const handleFetch = async () => {
+    setIsLoading(true)
+
+    await axios.get(`https://dummyjson.com/products/search?q=${search}`)
+    .then(response => setData(response.data.products))
+      
+    setIsLoading(false)
+  }
+
   useEffect(() => {
-    axios.get(`https://dummyjson.com/products/search?q=${search}`)
-    .then(response => setData(response.data.products) )
+    
+    handleFetch()
   }, [])
 
+  
   return (
-    <div className="mt-20 flex flex-col items-center gap-10">
-      <h1 className="text-3xl">Showing search results for : {search}</h1>
+    <>
+      {isLoading && (
+        <div className="mt-20 w-screen flex justify-center">
+          <Loading />
+        </div>
+      )}
 
-
-      <div className="w-5/6 flex gap-4">
-      {data?.map(product => (
-        <ProductCard title={product.title} price={product.price} thumbnail={product.thumbnail} brand={product.brand} rating={product.rating} />
-      ))}
-      </div>
-    </div>
+      {!isLoading && (
+        <main className="mt-20 flex flex-col items-center gap-10">
+          <h1 className="text-3xl">Showing search results for : {search}</h1>
+    
+    
+          <div className="w-5/6 flex gap-10 flex-wrap items-center ">
+          {data?.map(product => (
+            <ProductCard title={product.title} price={product.price} thumbnail={product.thumbnail} brand={product.brand} rating={product.rating} />
+          ))}
+          </div>
+        </main>
+      )}
+    </>
   )
 }
 
