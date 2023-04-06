ee0d8db736a388e4b76b7e6023c87b1d9829a746
Lucas Pinheiro <lps1704@gmail.com>
2023-04-06 16:47:47 -0300

    define main structure of product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -19,11 +19,12 @@ interface ProductsType {
 }
 
 const page = () => {
-
     // getting id like this while useParams doesn't work
     const path = usePathname()
     const id = path.replace('/product/', '')
-    const [data, setData] = useState<ProductsType | null >(null)  
+
+    const [data, setData] = useState<ProductsType | null>(null)
+    const [activeImage, setActiveImage] = useState("a")
 
     const handleFetch = async () => {
         await axios.get(`https://dummyjson.com/products/${id}`)
@@ -33,11 +34,41 @@ const page = () => {
     useEffect(() => {
         handleFetch()
     }, [])
+
+    useEffect(() => {
+        if (data !== null) setActiveImage(data.images[0])
+    }, [data])
+
+
   return (
-      <div className="mt-20">
+      <div className="h-screen w-screen flex justify-evenly items-center">
           
-          <h1>{data?.title}</h1>
-          <p>{data?.description}</p>
+          <div className="flex items-center gap-8">
+            <div>
+                {data?.images.map(image => (
+                    <img
+                        src={image}
+                        alt={`${data.title} Image`}
+                        className="h-12 w-12 cursor-pointer py-2"
+                        onClick={() => setActiveImage(image)}
+                    />
+                ))}
+            </div>
+            <img src={activeImage} alt={`${data?.title} Image`} className="h-96 w-96"/>
+          </div>
+          <div className="flex flex-col gap-5 w-1/3">
+              <h1 className="text-3xl font-semibold">{data?.title}</h1>
+              <p className="text-xl text-gray-500 font-semibold line-through"> ${data?.price},00</p>
+              {data?.price && data.discountPercentage && (
+                  <p className="text-xl text-blue-500 font-semibold">
+                     $ {Math.round(data?.price - data?.price * (data?.discountPercentage / 100))},00
+                  </p>
+              )}
+              <p>{data?.description}</p>
+              <p>{data?.brand}</p>
+              <button className="bg-blue-500 text-white uppercase p-5 w-1/3">Add to cart</button>
+          </div>
+
 
     </div>
   )
