cceff259faf0763b1b69384f053760129b69e1e9
Lucas Pinheiro <lps1704@gmail.com>
2023-04-20 11:34:45 -0300

    fix responsiveness

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -7,6 +7,7 @@ import {CartContext} from '../../context/CartContext'
 import Loading from '../../loading'
 import {TotalPriceContext} from '../../context/TotalPriceContext'
 import ProductsByCategorie from '../../components/ProductsByCategorie'
+import { BsCartPlus } from 'react-icons/bs'
 
 interface ProductsType {
     id: number,
@@ -59,6 +60,10 @@ const page = () => {
         if (cart.length === 0) setAlreadyOnCart(false)
     }, [cart, data])
 
+    useEffect(() => {
+        window.scroll(0,0)
+    }, [data?.title])
+
   return (
       <>
           {isLoading && (
@@ -68,29 +73,30 @@ const page = () => {
           )}
           
           {!isLoading && (
-            <div className="min-h-screen w-screen flex flex-col items-center py-12">
+            <div className="min-h-screen w-screen flex flex-col items-center md:py-12 py-32 gap-24">
           
-              <div className="h-screen w-screen flex justify-around items-center">
+              <div className="h-screen w-screen flex xl:justify-around justify-evenly items-center flex-col xl:flex-row gap-12 md:gap-0">
                 <img     
                 src={data?.image}
                 alt={`${data?.title} Image`}
-                className="h-80 w-80 cursor-pointer py-2"
+                className="md:h-80 md:w-80  h-44 w-44 cursor-pointer py-2"
                 />
-                <div className="flex flex-col gap-5 w-1/3">
-                        <h1 className="text-3xl text-strongRed font-semibold">{data?.title}</h1>
-                        <p>{data?.description}</p>
-                    <p className="text-xl text-black font-semibold"> ${data?.price}</p>
+                <div className="flex flex-col gap-8 2xl:w-1/3 xl:w-1/2 w-full p-8">
+                        <h1 className="xl:text-3xl sm:text-2xl text-lg text-strongRed font-semibold">{data?.title}</h1>
+                        <p className="text-lg">{data?.description}</p>
+                    <p className="text-2xl text-black font-bold"> ${data?.price}</p>
                     <button
-                        onClick={() => addToCart()}
-                        className="bg-strongRed text-white uppercase p-5 w-1/3"
-                    >
-                        Add to cart
+                    onClick={() => addToCart()}
+                    className="flex justify-center items-center gap-4 xl:w-2/5  sm:w-1/2 w-full p-4 bg-strongRed"
+                    >       
+                        <BsCartPlus fill="white" size={32} />
+                        <p className="text-white uppercase font-semibold">Add to cart</p>
                     </button>
                 </div>
               </div>
                   
-                <div className="flex flex-col w-3/5">
-                    <h1 className="text-5xl capitalize text-strongRed ml-24">Similar Products</h1>
+                <div className="flex flex-col xl:w-4/5 w-full p-2">
+                    <h1 className="xl:text-5xl text-2xl  text-strongRed xl:ml-24 ml-12 uppercase font-semibold">Similar Products</h1>
                     {data && <ProductsByCategorie categorie={data?.category} />} 
                 </div>
           </div>
